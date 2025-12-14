e50d0b6954d06604d5a5542cd5cb0ed2e7c32bc3
Tomasz Kokoszka <tkokoszka@gmail.com>
2025-12-15 00:47:49 +0100

    do not carry results

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
+import humanize
+
 from dataclass_regular import create_dataclasses
 from dataclass_slots import create_dataclasses_slots
 from measure_res import measure
 
 
 def main():
+    num_cycles = 1_000_000
+
     def _fun1():
-        return create_dataclasses(10_000_000)
+        nonlocal num_cycles
+        return create_dataclasses(num_cycles)
 
     def _fun2():
-        return create_dataclasses_slots(10_000_000)
+        nonlocal num_cycles
+        return create_dataclasses_slots(num_cycles)
 
-    r1 = measure("regular", _fun1)
-    r2 = measure("slots", _fun2)
-    print(f"r1 len={len(r1)}, r2 len={len(r2)}")
+    print(f"### Running {humanize.intcomma(num_cycles)} cycles")
+    measure("slots", _fun2)
+    measure("regular", _fun1)
 
 
 if __name__ == "__main__":
