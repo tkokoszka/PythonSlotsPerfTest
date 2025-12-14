d8c5a5d0951414536892a22dbf99fdb0912e28ac
Tomasz Kokoszka <tkokoszka@gmail.com>
2025-12-15 00:49:39 +0100

    main.py: use representative function names

--- src/main.py
+++ src/main.py
@@ -8,17 +8,17 @@ from measure_res import measure
 def main():
     num_cycles = 1_000_000
 
-    def _fun1():
+    def _fun_dataclass():
         nonlocal num_cycles
         return create_dataclasses(num_cycles)
 
-    def _fun2():
+    def _fun_dataclass_slots():
         nonlocal num_cycles
         return create_dataclasses_slots(num_cycles)
 
     print(f"### Running {humanize.intcomma(num_cycles)} cycles")
-    measure("slots", _fun2)
-    measure("regular", _fun1)
+    measure("dataclass_slots", _fun_dataclass_slots)
+    measure("dataclass", _fun_dataclass)
 
 
 if __name__ == "__main__":
