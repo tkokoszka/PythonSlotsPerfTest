3e462a4bcb4bfe44db86904c1daba3387e121439
Tomasz Kokoszka <tkokoszka@gmail.com>
2026-01-18 00:16:11 +0100

    explain the level name

--- src/main.py
+++ src/main.py
@@ -28,8 +28,8 @@ def main():
 def configure_logger():
     logging.basicConfig(
         level=logging.INFO,
-        # .1s takes the first character of the level name (I, W, E, etc.)
-        # .%(msecs)03d prints millisecs, something that datefmt does not offer
+        # %(levelname).1s takes the first character of the level name (I, W, E, etc.)
+        # %(msecs)03d prints millisecs, something that datefmt does not offer
         format="%(levelname).1s %(asctime)s.%(msecs)03d [%(name)s] %(message)s",
         # %f provides microsecond precision; we truncate/format as needed
         datefmt="%Y-%m-%d %H:%M:%S",
