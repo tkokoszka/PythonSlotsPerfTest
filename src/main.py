67631b949334ccc5e0925e25a6391582de95b0fd
Tomasz Kokoszka <tkokoszka@gmail.com>
2026-01-19 23:18:31 +0100

    chore: Remove outdated coment

--- src/main.py
+++ src/main.py
@@ -31,7 +31,6 @@ def configure_logger():
         # %(levelname).1s takes the first character of the level name (I, W, E, etc.)
         # %(msecs)03d prints millisecs, something that datefmt does not offer
         format="%(levelname).1s %(asctime)s.%(msecs)03d [%(name)s] %(message)s",
-        # %f provides microsecond precision; we truncate/format as needed
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
